ef8b2ed540095ce6900b2ff47b2ee6852b8d4c08
K-Reversed <99205481+K-Reversed@users.noreply.github.com>
2025-02-04 17:28:30 -0500

    Cleaning up Program

--- assigns/01/MySolution/Payroll.java
+++ assigns/01/MySolution/Payroll.java
@@ -1,19 +1,17 @@
 /**
  * @author Kevin Jiang
  * @author Hongwei Xi
- * @version  v1.0, Feb 2 2025
+ * @version  v1.11, 4 Feb 2025
  */
-
-import java.util.Arrays;
-import java.util.ArrayList;
-
+import java.text.DecimalFormat;
 public class Payroll {
     public static final int INITIAL_MAXIMUM_SIZE = 1024;
+    DecimalFormat df = new DecimalFormat("#.00");
 
     public Payroll() {
-        int payroll = 20;
         maximum_size = INITIAL_MAXIMUM_SIZE;
         current_size = 0;
+        people = new Employee[maximum_size];
     }
 
     /**
@@ -25,17 +23,21 @@ public class Payroll {
     }
 
     public void print(){
-        for (Employee person : people) {
-            System.out.println(person);
-        }
+        try {
+            for (Employee person : people) {
+                System.out.println("[" + person.name + ", #" + person.ID + ", $" + df.format(person.salary) + "]");
+            }
+        } catch (NullPointerException _) {}
     }
 
     public void add_employee(Employee newbie) {
-
+        people[current_size] = newbie;
+        System.out.println(newbie.name + " added with id #" + newbie.ID + ", salary is $" + df.format(newbie.salary));
+        current_size++;
     }
 
     public void remove_employee(int i) throws EmployeeIndexException {
-        if (i > people.length - 1) {
+        if (i > people.length - 1 || people[i] == null) {
             throw new EmployeeIndexException();
         }
     }
@@ -44,7 +46,7 @@ public class Payroll {
         boolean employeeFound = false;
         int employeeID = 0;
         for (int i = 0; i < people.length; i++) {
-            if (people[i].toString().equals(name)) {
+            if (people[i].name.equals(name)) {
                 System.out.println("Employee " + name + " Found!");
                 employeeFound = true;
                 employeeID = i;
--- assigns/01/MySolution/TestPayroll.java
+++ assigns/01/MySolution/TestPayroll.java
@@ -1,96 +1,58 @@
 /**
  * @author Kevin Jiang
  * @author Hongwei Xi
- * @version  v1.0, Feb 2 2025
+ * @version  v1.11, Feb 2 2025
  */
 
 import java.io.IOException;
-import java.util.InputMismatchException;
+import java.text.DecimalFormat;
+import java.util.Random;
 import java.util.Scanner;
-import java.util.concurrent.TimeUnit;
 
 public class TestPayroll {
     public static void main(String[] args) throws InterruptedException, IOException {
         Scanner input = new Scanner(System.in);
+        Random r = new Random();
+        int upper = 60000;
+        int lower = 20000;
 
-        Payroll payroll = new Payroll();
+        Payroll payrollA = new Payroll();
+        Payroll payrollB = new Payroll();
 
-        String inputNumber;
-        String name;
+        String[] namesA = {"A", "B", "C", "D", "E", "F"};
+        String[] namesB = {"Alpha", "Bravo", "Charlie", "Delta", "Echo", "Foxtrot", "Golf"};
+
+        for (String name : namesA) {
+            Employee e = new Employee();
+            e.name = name;
+            e.ID = payrollA.size() + 1;
+            e.salary = r.nextDouble(upper - lower) + lower;
+            payrollA.add_employee(e);
+        }
+
+        for (String name : namesB) {
+            Employee e = new Employee();
+            e.name = name;
+            e.ID = payrollB.size() + 1;
+            e.salary = r.nextDouble(upper - lower) + lower;
+            payrollB.add_employee(e);
+        }
+
+        System.out.println("__________________________________________________");
+        payrollA.print();
+
+        System.out.println("__________________________________________________");
+        payrollB.print();
+
         int selection;
-        int id;
-
         do {
             System.out.println("""
-                    What would you like to do?
-                    1. View number of employees
-                    2. Print all employee information
-                    3. Add an employee
-                    4. Remove an employee
-                    5. Find an employee
-                    6. Copy payroll
-                    7. Add payroll
-                    8. Terminate Program""");
-            System.out.print("Please enter a valid number: ");
-            id = 0;
-            inputNumber = input.next();
+                    Choose an action:
+                    1. 
+                    """);
+        } while (false);
 
-            try {
-                selection = Integer.parseInt(inputNumber);
-            } catch (NumberFormatException e) {
-                System.out.println("The inputted value is not a number, please try again.");
-                TimeUnit.SECONDS.sleep(2);
-                continue;
-            }
-            switch (selection) {
-                case 1:
-                    System.out.println(payroll.size());
-                    System.out.println("Press enter to continue:");
-                    System.in.read();
-                    continue;
-                case 2:
-                case 3:
-                case 4:
-                    try {
-                        System.out.print("Please enter the employee's id: ");
-                        id = input.nextInt();
-                        payroll.remove_employee(id);
-                    } catch (EmployeeIndexException e) {
-                        System.out.println("Employee not found!");
-                        System.out.println("Press enter to continue:");
-                        System.in.read();
-                        continue;
-                    } catch (InputMismatchException e) {
-                        System.out.println("Not a number!");
-                        System.out.println("Press enter to continue:");
-                        System.in.read();
-                        continue;
-                    }
-                case 5:
-                    name = input.next("Please enter the employee's name");
-                    try {
-                        id = payroll.find_employee(name);
-                        System.out.println("Employee ID: " + id);
-                        System.out.println("Press enter to continue:");
-                        System.in.read();
-                        continue;
-                    } catch (EmployeeNotFoundException e) {
-                        System.out.println("Employee not found!");
-                        System.out.println("Press enter to continue:");
-                        System.in.read();
-                        continue;
-                    }
-                case 6:
-                case 7:
-                    System.out.println("7");
-                case 8:
-                    System.out.println("Terminating program");
-                    TimeUnit.SECONDS.sleep(2);
-                    System.exit(0);
-                default:
-                    System.out.println("The number is outside the program range, please input a different number.");
-                    TimeUnit.SECONDS.sleep(2);
-            }
-        } while (true);
+
+        input.close();
     }
 }
