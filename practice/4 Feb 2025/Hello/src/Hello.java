b86d52f2fa4184c2ce8a4b1e1e95ab4f174b9443
K-Reversed <99205481+K-Reversed@users.noreply.github.com>
2025-02-20 20:06:13 -0500

    Untrack files in .gitignore

--- practice/4 Feb 2025/Hello/src/Hello.java
+++
@@ -1,7 +0,0 @@
-import edu.princeton.cs.algs4.*;
-public class Hello {
-    public static void main(String[] args) {
-        //System.out.println("Hello, World!");
-        StdOut.println("Hello, World!");
-    }
-}
