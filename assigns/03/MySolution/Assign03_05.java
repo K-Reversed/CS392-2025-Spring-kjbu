409ad291838801bff0e29329daad3c93e1056a0a
K-Reversed <99205481+K-Reversed@users.noreply.github.com>
2025-03-02 22:54:19 -0500

    Assigns03_05 Updated

--- assigns/03/MySolution/Assign03_05.java
+++ assigns/03/MySolution/Assign03_05.java
@@ -1,37 +1,48 @@
+import java.util.Arrays;
 import java.util.Comparator;
 
-public
-class
-Assign03_05 <T extends Comparable<T>> implements ArraySorter<T> {
-    private static int n;
-
+@SuppressWarnings({"rawtypes", "unchecked"})
+public class Assign03_05 <T extends Comparable<T>> implements ArraySorter<T> {
     public static void main(String[] args) {
-        String[] strings = {"Alpha", "Bravo", "Charlie", "Delta", "Echo", "Foxtrot", "Gulf"};
-        n = strings.length;
+        String[] strings = {"Gulf", "Foxtrot", "Echo", "Delta", "Charlie", "Bravo", "Alpha"};
+        System.out.println(Arrays.toString(strings));
+        var test = new Assign03_05();
+        test.ArraySorter().sort(strings);
+        System.out.println(Arrays.toString(strings));
     }
 
     @Override
     public void sort(T[] A) {
 	// please give a RECURSIVE implementation of INSERTION sort
+        recursion(A, A.length);
+    }
 
-        if (n <= 1) {
+    private void recursion (T[] A, int len) {
+        if (len <= 1) {
             return;
         }
-        n--;
-        sort(A);
-
-        T bottom = A[n];
-        int j = n - 1;
-
-        while (j >= 0 && A[j].compareTo(bottom) > 0) {
+        recursion(A, len - 1);
+        T last = A[len - 1];
+        int j = len - 2;
+        System.out.println(new CompareString<T>().compare(A[j], last));
+        while ((j >= 0) && new CompareString<T>().compare(A[j], last) > 0) {
             A[j + 1] = A[j];
             j--;
         }
-        A[j + 1] = bottom;
+        A[j + 1] = last;
+        System.out.println(Arrays.toString(A));
     }
 
     public ArraySorter<T> ArraySorter() {
 	// [ArraySorter] wrap the method [sort] inside an object
-        return null;
+        return Assign03_05.this;
+    }
+}
+
+class CompareString<T> implements Comparator<T> {
+
+    @Override
+    public int compare(T o1, T o2) {
+        return o1.toString().compareToIgnoreCase(o2.toString());
     }
 }
