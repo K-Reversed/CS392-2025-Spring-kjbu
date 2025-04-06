bc0155e9b8da1b8e09e0f39849ac07e967629a32
K-Reversed <99205481+K-Reversed@users.noreply.github.com>
2025-04-06 12:38:24 -0400

    Update to Assigns04_04 + Makefile added

--- assigns/04/MySolution/Assign04_04.java
+++ assigns/04/MySolution/Assign04_04.java
@@ -9,33 +9,49 @@ import java.util.Comparator;
 /**
  * @since v1.0
  */
+@SuppressWarnings({"unchecked", "UnnecessaryBoxing"})
 public class Assign04_04 {
 
     public static void main(String[] argv) {
         // Please provide some testing code here
-        OInt[] oInts = {
-                new OInt(10),
-                new OInt(10),
-                new OInt(9),
-                new OInt(8),
-                new OInt(7),
-                new OInt(6),
-                new OInt(6),
-                new OInt(5),
-                new OInt(4),
-                new OInt(3),
-                new OInt(2),
-                new OInt(1)
-        };
 
-        Integer[] integers = {10, 10, 9, 8, 7, 6, 6, 5, 4, 3, 2, 1};
+        Integer[] integers = new Integer[]{
+                Integer.valueOf(10),
+                Integer.valueOf(10),
+                Integer.valueOf(9),
+                Integer.valueOf(8),
+                Integer.valueOf(7),
+                Integer.valueOf(6),
+                Integer.valueOf(6),
+                Integer.valueOf(5),
+                Integer.valueOf(4),
+                Integer.valueOf(3),
+                Integer.valueOf(2),
+                Integer.valueOf(1),
+        };
+        String[] strings = {
+                new String("z"),
+                new String("cat"),
+                new String("cat"),
+                new String("cat"),
+                new String("r"),
+                new String("y"),
+                new String("p"),
+                new String("p"),
+                new String("q"),
+                new String("g"),
+                };
         System.out.println(Arrays.toString(integers));
         Collections.shuffle(Arrays.asList(integers));
         System.out.println(Arrays.toString(integers));
         stableSort(integers);
-        //stableSort(oInts);
+        System.out.println(Arrays.toString(strings));
+        Collections.shuffle(Arrays.asList(strings));
+        System.out.println(Arrays.toString(strings));
+        stableSort(strings);
 
         System.out.println(Arrays.toString(integers));
+        System.out.println(Arrays.toString(strings));
     }
 
     // This class should not be instantiated.
@@ -51,91 +67,136 @@ public class Assign04_04 {
 	// stable sorting function directly.)
 
         var sort = new ArrSort<T>();
+        int index = 0;
+        int[] dupes = new int[A.length * 2];
 
-        //returns an unstable sorted array
+        for (int i = 0; i < A.length - 1; i++) {
+            for (int j = i + 1; j < A.length; j++) {
+                if (A[i].toString().equals(A[j].toString())) {
+                    dupes[index] = System.identityHashCode(A[i]);
+                    index++;
+                    dupes[index] = System.identityHashCode(A[j]);
+                    index++;
+                    System.out.println(A[i] + ":" + System.identityHashCode(A[i]) + ", " + A[j] + ":" + System.identityHashCode(A[j]));
+                }
+            }
+        }
+
+        //returns a sorted array
         sort.sort(A);
 
-
+        for (int i = 0; i < A.length - 1; i++) {
+            int tmp = System.identityHashCode(A[i]);
+            int tmp1 = System.identityHashCode(A[i + 1]);
+            for (int j = 0; j < dupes.length; j++) {
+                if (dupes[j] == 0) {
+                    break;
+                }
+                if (tmp == dupes[j]) {
+                    if (tmp1 == dupes[j + 1]) {
+                        System.out.println(A[i] + " " + tmp1 + " : " + A[i + 1] + " " + dupes[j + 1]);
+                    }
+                }
+            }
+        }
     }
 
-    static class OInt {
-        private final Integer id;
-        OInt(Integer id) {
-            this.id = id;
+    @SuppressWarnings("unchecked")
+    static class LinkedList<T> {
+        private int size;
+        private LList<T> top;
+
+        public void insert(T x) {
+            var data = new LList<>(x);
+            data.next = null;
+            if (top == null) {
+                top = data;
+            } else {
+                LList<T> tmp = top;
+                while (tmp.next != null) {
+                    tmp = tmp.next;
+                }
+                tmp.next = data;
+            }
+            size++;
         }
 
-        public String toString() {
-            return String.valueOf(this.id);
+        public T peek(int index) {
+            LList<T> tmp = top;
+            for (int i = 0; i < index; i++) {
+                tmp = tmp.next;
+            }
+            return tmp.elem;
+        }
+
+        public T[] toArray() {
+            T[] array = (T[]) new Object[size];
+            for (int i = 0; i < size; i++) {
+                array[i] = peek(i);
+            }
+            return array;
         }
     }
 
+    @SuppressWarnings("unchecked")
     static class ArrSort<T extends Comparable<T>> implements ArraySorter04<T> {
         @Override
         public void sort(T[] A) {
-            int right = A.length;
-            sortRecursion(A, 0, right);
-        }
+            if (A.length <= 1) return;
+            boolean sorted = false;
+            for (int i = 0; i < A.length - 1; i++) {
+                if ((A[i].compareTo(A[i + 1]) <= 0)) {
+                    sorted = true;
+                } else {
+                    sorted = false;
+                    break;
+                }
+            }
+            if (sorted) {
+                return;
+            }
+            final int mid = getPivot(0, A.length - 1);
+            final T pivot = A[mid];
+            System.out.println(mid + ", " + pivot);
+            var lesser = new LinkedList<>();
+            var greater = new LinkedList<>();
 
-        private void sortRecursion (T[] A, int left, int right) {
-            if (right <= left + 1) return;
-            final int pivot = getPivot(left, right - 1);
-            exchange(A, pivot, right - 1);
-            final int mid = split(A, left, right);
-            exchange(A, mid, right - 1);
-            sortRecursion(A, left, mid);
-            sortRecursion(A, mid + 1, right);
+            for (int i = 0; i < A.length; i++) {
+                if (i != mid) {
+                    if (A[i].compareTo(pivot) < 0) {
+                        lesser.insert(A[i]);
+
+                    } else if (A[i].compareTo(pivot) > 0) {
+                        greater.insert(A[i]);
+
+                    } else {
+                        if (i < mid) {
+                            lesser.insert(A[i]);
+                        } else {
+                            greater.insert(A[i]);
+                        }
+                    }
+                    System.out.println(Arrays.toString(lesser.toArray()) + " " + pivot + " " + Arrays.toString(greater.toArray()));
+                }
+            }
+
+            int size = 0;
+            for (int i = 0; i < lesser.toArray().length; i++) {
+                A[size] = (T) lesser.toArray()[i];
+                size ++;
+            }
+            A[size] = pivot;
+            size ++;
+            for (int i = 0; i < greater.toArray().length; i++) {
+                A[size] = (T) greater.toArray()[i];
+                size ++;
+            }
+
+            sort(A);
         }
 
         private int getPivot(int left, int right) {
             return (int) Math.floor(Math.random() * (right - left + 1) + left);
-        }
-
-        private static <T extends Comparable<T>> void exchange(T[] A, int i, int j) {
-            T tmp;
-            tmp = A[i];
-            A[i] = A[j];
-            A[j] = tmp;
-        }
-
-        private static <T extends Comparable<T>> int split(T[] A, int left, int right) {
-            int pointer = left;
-            T pivot = A[right - 1];
-            for (int j = left; j <= right; j++) {
-                if (less(A[pointer], pivot)) {
-                    pointer += 1;
-                } else break;
-            }
-            return splitRecursion(A, pointer, pointer + 1, pivot);
-        }
-
-        private static <T extends Comparable<T>> int splitRecursion(T[] A, int left, int right, T pivot) {
-            var c = new Comparator<T>() {
-                @Override
-                public int compare(T o1, T o2) {
-                    return o1.compareTo(o2);
-                }
-            };
-
-            if (right < A.length - 1) {
-                if (less(A[right], pivot)) {
-                    System.out.println("less");
-                    exchange(A, left, right);
-                    return splitRecursion(A, left + 1, right + 1, pivot);
-                } else if (c.compare(A[right], pivot) == 0) {
-                    System.out.println("equal: " + A[left]);
-                    exchange(A, left, right);
-                    return splitRecursion(A, left + 1, right + 1, pivot);
-                } else {
-                    System.out.println("more");
-                    return splitRecursion(A, left, right + 1, pivot);
-                }
-            }
-            System.out.println(Arrays.toString(A));
-            return left;
-        }
-
-        private static <T extends Comparable<T>> boolean less (T x, T y) {
-            return (x.compareTo(y) < 0);
         }
     }
 }
